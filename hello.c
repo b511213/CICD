e402b1591402aa8d9b79b3b353e8a073f6fd1e10
b511213 <80499133+b511213@users.noreply.github.com>
2021-05-30 23:17:41 +0900

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(){
-    pirntf("<html>\n");
+    printf("<html>\n");
     printf("<h1>Hello!!! wow</h1>\n");
-    pirntf("</html>\n");
+    printf("</html>\n");
     return 0;
 }
